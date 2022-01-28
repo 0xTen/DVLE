8ec1a3f52c091acbdee2bc3a5ea13b1ec65fb425
0xTen <70155332+0xTen@users.noreply.github.com>
2022-01-28 15:01:16 -0300

    fix bad scanfs, cleaner code and add README.md

---
+++ README.md
@@ -0,0 +1,12 @@
+# About this project
+DVLE is a simple linux elf executable packed with a TON of memory corruption vulnerabilities.
+
+# Compilation
+To compile this project simply run:
+
+`gcc dvle.c -o dvle`
+
+# Usage
+To use this projet execute:
+
+`./dvle`
--- dvle.c
+++ dvle.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
- 
+
+#define banner "                                                                                            \n                                                                                            \nDDDDDDDDDDDDD      VVVVVVVV           VVVVVVVVLLLLLLLLLLL             EEEEEEEEEEEEEEEEEEEEEE\nD::::::::::::DDD   V::::::V           V::::::VL:::::::::L             E::::::::::::::::::::E\nD:::::::::::::::DD V::::::V           V::::::VL:::::::::L             E::::::::::::::::::::E\nDDD:::::DDDDD:::::DV::::::V           V::::::VLL:::::::LL             EE::::::EEEEEEEEE::::E\n  D:::::D    D:::::DV:::::V           V:::::V   L:::::L                 E:::::E       EEEEEE\n  D:::::D     D:::::DV:::::V         V:::::V    L:::::L                 E:::::E             \n  D:::::D     D:::::D V:::::V       V:::::V     L:::::L                 E::::::EEEEEEEEEE   \n  D:::::D     D:::::D  V:::::V     V:::::V      L:::::L                 E:::::::::::::::E   \n  D:::::D     D:::::D   V:::::V   V:::::V       L:::::L                 E:::::::::::::::E   \n  D:::::D     D:::::D    V:::::V V:::::V        L:::::L                 E::::::EEEEEEEEEE   \n  D:::::D     D:::::D     V:::::V:::::V         L:::::L                 E:::::E             \n  D:::::D    D:::::D       V:::::::::V          L:::::L         LLLLLL  E:::::E       EEEEEE\nDDD:::::DDDDD:::::D         V:::::::V         LL:::::::LLLLLLLLL:::::LEE::::::EEEEEEEE:::::E\nD:::::::::::::::DD           V:::::V          L::::::::::::::::::::::LE::::::::::::::::::::E\nD::::::::::::DDD              V:::V           L::::::::::::::::::::::LE::::::::::::::::::::E\nDDDDDDDDDDDDD                  VVV            LLLLLLLLLLLLLLLLLLLLLLLLEEEEEEEEEEEEEEEEEEEEEE\n                                                                                            \n                                                                                            \n                                                                                            \n                                                                                            \n                                                                                            \n                                                                                            \n                                                                                            "
+
 extern char** environ;
-char overflowme[20];
 struct data {
     char buf[1024];
 };
 struct data *chunks[60] = {NULL};
 int counter = 0;
- 
+
 int alloc(){
     int option;
     int size;
     int idx;
     printf("1 - malloc\n2 - calloc\n3 - realloc\n\n> ");
-    scanf("%d",&option);
+    scanf("%d", &option);
     switch(option){
         case 1:
             printf("size: ");
@@ -37,77 +38,76 @@ int alloc(){
         default:
             printf("No such option\n\n");
             return counter;
- 
+
     }
     counter = counter + 1;
     printf("New Chunk added\n\n");
     return counter;
 }
- 
+
 void edit(){
     int idx;
-    char data[1024];
     printf("index: ");
-    scanf("%d",&idx);
+    scanf("%d", &idx);
     printf("data: ");
-    scanf("%s",(char *)data);char overflowme[20];
-    strcpy(chunks[idx]->buf,data);
+    gets(chunks[idx]->buf);
     printf("Chunk edited\n\n");
 }
- 
+
 void dump(){
     int idx;
     printf("index: ");
-    scanf("%d",&idx);
-    printf("data:\n\n%s\n\n",chunks[idx]->buf);
+    scanf("%d", &idx);
+    printf("data:\n\n%s\n\n", chunks[idx]->buf);
 }
- 
+
 void delete(){
     int idx;
     printf("index: ");
-    scanf("%d",&idx);
+    scanf("%d", &idx);
     free(chunks[idx]->buf);
     printf("Chunk deleted\n");
 }
- 
+
 void show_ptr(){
     int idx;
     printf("index: ");
-    scanf("%d",&idx);
-    printf("%d: %p\n\n",idx,chunks[idx]);
+    scanf("%d", &idx);
+    printf("%d: %p\n\n", idx, chunks[idx]);
 }
- 
+
 void echo(){
     char in[1024];
     printf("string: ");
-    scanf("%s",in);
+    scanf("%s", in);
     printf(in);
     printf("\n");
 }
- 
+
 void shellz(){
-    char * bin_sh = "/bin/sh";
-    system(bin_sh);
+    system("/bin/sh");
 }
- 
+
 void leak_libc(){
-    printf("puts() @ %p\n\n",puts);
+    printf("puts() @ %p\n\n", puts);
 }
- 
+
 void leak_elf(){
-    printf("shellz() @ %p\n\n",shellz);
+    printf("shellz() @ %p\n\n", shellz);
 }
- 
+
 void leak_stack(){
-    printf("environ @ %p\n\n",environ);
+    printf("environ @ %p\n\n", environ);
 }
- 
+
 void stack_overflow(){
+    char overflowme[20];
     printf("overflow me: ");
-    scanf("%s",&overflowme[0]);
+    gets(overflowme);
 }
- 
+
 int main(){
+    printf("%s", banner);
     int option;
     while (1){
         printf(
@@ -164,7 +164,7 @@ int main(){
                 return 0;
             default:
                 printf("No such option\n\n");
- 
+
         }
     }
     printf("Error!");
